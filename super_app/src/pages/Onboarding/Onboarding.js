52f8f90f7527bb88760882b55adbdd99757efd8f
Mohammed Nushoor Khan <auferstehung20@gmail.com>
2023-03-07 22:18:52 +0530

    updated onboarding page

--- super_app/src/pages/Onboarding/Onboarding.js
+++ super_app/src/pages/Onboarding/Onboarding.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import CategoryChip from "../../components/CategoryChip/CategoryChip"
 import ButtonComponents from "../../components/ButtonComponents/BtnComponents"
 import styles from "./Onboarding.module.css"
@@ -6,67 +6,80 @@ import styles from "./Onboarding.module.css"
 import OnboardingStates from "./OnboardingStates"
 
 
-export default 
-function OnboardingPage(){
-    
-    const [selected, setSelected] = React.useState(OnboardingStates);
+export default
+    function OnboardingPage() {
+
+
+    const [selected, setSelected] = React.useState(
+        localStorage.getItem('selectedCards') ?
+            JSON.parse(localStorage.getItem('selectedCards')) :
+            OnboardingStates
+    );
     const [categorySelected, setCategorySelected] = React.useState(false);
 
-    const Cards = selected.map((card)=>{
-        return (<CategoryChip 
-            key = {card.id}
-            id = {card.id} 
-            title = {card.title}
-            img = {card.img}
-            color = {card.color}
-            on = {card.on}
-            customClick = {handleClick}
+    const Cards = selected.map((card) => {
+        return (<CategoryChip
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            img={card.img}
+            color={card.color}
+            on={card.on}
+            customClick={handleClick}
         />)
     })
-   
-    const Buttons = selected.filter((card)=>{
-        if(card.on == true){
+
+
+
+    const Buttons = selected.filter((card) => {
+        if (card.on == true) {
             return card;
         }
     })
 
-    const displayButtons = Buttons.map((card)=>{
-        return(
-            <ButtonComponents 
+
+    const displayButtons = Buttons.map((card) => {
+        return (
+            <ButtonComponents
                 key={card.id}
-                id = {card.id}
-                title = {card.title}
-                customClick = {handleClick}
+                id={card.id}
+                title={card.title}
+                customClick={handleClick}
             />
         )
     })
 
-    function handleClick(id){
-        console.log("clicked" , id);
-        
-        setSelected((prevSelected)=>{
+    function handleClick(id) {
+        console.log("clicked", id);
+
+        setSelected((prevSelected) => {
             return (
-                prevSelected.map((card)=>{
-                    return card.id === id ? {...card, on : !card.on} : card
+                prevSelected.map((card) => {
+                    return card.id === id ? { ...card, on: !card.on } : card
                 })
             )
         })
-        
+
     }
-    React.useEffect(()=>{
-        localStorage.setItem('selectedCards', JSON.stringify(Buttons));
-        console.log(displayButtons);
-        if(displayButtons.length > 0){
+
+    React.useEffect(() => {
+        // if(localStorage.getItem('selectedCards'))
+        localStorage.setItem('selectedCards', JSON.stringify(selected));
+        // console.log(selected);
+    }, [selected])
+
+    React.useEffect(() => {
+        if (displayButtons.length > 0) {
             console.log("success");
             setCategorySelected(false);
         }
-        else{
+        else {
             console.log("Select atleast 1");
             setCategorySelected(true);
         }
     }, [displayButtons])
 
-    return(
+    return (
         <div className={styles.main}>
             <div className={styles.top}>
                 <div className={styles.left}>
@@ -75,32 +88,32 @@ function OnboardingPage(){
                         <h2 className={styles.text2}>Choose your entertainment category</h2>
                         {categorySelected && <p className={styles.text3}>Select atleast one category</p>}
                     </div>
-                    <div className = {styles.leftBottom}>
+                    <div className={styles.leftBottom}>
                         {/* Component buttons here*/}
-                       
+
                         {displayButtons}
-                        
+
                     </div>
                 </div>
                 <div className={styles.right}>
-                        {/* Components chips here */}
-                       {Cards}
+                    {/* Components chips here */}
+                    {Cards}
                 </div>
             </div>
             <div className={styles.bottom}>
-                    <button className={styles.BtnNextPage}
-                        onClick = {()=>{
-                            if(displayButtons.length > 0){
+                <button className={styles.BtnNextPage}
+                    onClick={() => {
+                        if (displayButtons.length > 0) {
 
-                                console.log("success");
-                                setCategorySelected(false);
-                            }
-                            else{
-                                console.log("Select atleast 1");
-                                setCategorySelected(true);
-                            }
-                        }}
-                    >Next Page</button>
+                            console.log("success");
+                            setCategorySelected(false);
+                        }
+                        else {
+                            console.log("Select atleast 1");
+                            setCategorySelected(true);
+                        }
+                    }}
+                >Next Page</button>
             </div>
         </div>
     )
