5cd4726c1e87be98fa566063c4b0bc86dc12aca0
Mohammed Nushoor Khan <auferstehung20@gmail.com>
2023-03-09 23:47:15 +0530

    added timer card

--- super_app/src/pages/Profile/Profile.js
+++ super_app/src/pages/Profile/Profile.js
@@ -2,26 +2,40 @@ import React from "react";
 import ProfileComponent from "../../components/ProfileComponent/ProfileComponent";
 import WeatherApiComponent from "../../components/WeatherApiComponent/WeatherApiComponent";
 import NewsApiComponent from "../../components/NewsApiComponent/NewsApiComponent";
+import TimerComponent from "../../components/TimerComponent/TimerComponent";
 import styles from "./Profile.module.css"
 export default
     function ProfilePage() {
     return (
 
         <div className={styles.main}>
-            <div className={styles.minor1}>
+            <div className={styles.top}>
                 <div className={styles.left}>
-                    <div className={styles.top}>
-                        {<ProfileComponent />}
+                    <div className={styles.leftTop}>
+                        <div className={styles.others}>
+                            <div className={styles.profile}>
+                                {<ProfileComponent/>}
+                            </div>
+                            <div className={styles.weather}>
+                                {<WeatherApiComponent/>}
+                            </div>
+                        </div>
+                        <div className={styles.notes}>
+                            <h2>
+                            Underdevelopment !!!...
+                            </h2>
+                            
+                        </div>
                     </div>
-                    <div className={styles.bottom}>
-                        {<WeatherApiComponent />}
+                    <div className={styles.leftBottom}>
+                        {<TimerComponent/>}
                     </div>
                 </div>
                 <div className={styles.right}>
-                    {<NewsApiComponent />}
+                    {<NewsApiComponent/>}
                 </div>
             </div>
-            <div className={styles.minor2}>
+            <div className={styles.bottom}>
                 <button className={styles.button}>Next Page</button>
             </div>
         </div>
